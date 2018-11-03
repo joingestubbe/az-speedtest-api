6f0d68a146a542f0c0a226742f253db66fdeffc7
joingestubbe <jstubbe@gmail.com>
2018-11-03 16:31:36 +0100

    v2

--- SpeedTestApi/Controllers/SpeedTestController.cs
+++ SpeedTestApi/Controllers/SpeedTestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SpeedTestApi.Models;
 
 
 
@@ -19,39 +20,16 @@ namespace SpeedTestApi.Controllers
         {
             return Ok("PONG");
         }
+
+        // POST speedtest/
+        [HttpPost]
+        public ActionResult<string> UploadSpeedTest([FromBody] TestResult speedTest)
+        {
+            var speedTestData = $"Got a TestResult from { speedTest.User } with download { speedTest.Data.Speeds.Download } Mbps.";
+
+            return Ok(speedTestData);
+        }
+
     }
+
 }
-        
-
-        // [HttpGet]
-        // public ActionResult<IEnumerable<string>> Get()
-        // {
-        //     return new string[] { "value1", "value2" };
-        // }
-
-        // GET api/values/5
-        // [HttpGet("{id}")]
-        // public ActionResult<string> Get(int id)
-        // {
-        //     return "value";
-        // }
-
-        // // POST api/values
-        // [HttpPost]
-        // public void Post([FromBody] string value)
-        // {
-        // }
-
-        // // PUT api/values/5
-        // [HttpPut("{id}")]
-        // public void Put(int id, [FromBody] string value)
-        // {
-        // }
-
-        // // DELETE api/values/5
-        // [HttpDelete("{id}")]
-        // public void Delete(int id)
-        // {
-        // }
-//     }
-// }
